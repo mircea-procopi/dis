3d1b30eb21a56b577f77d5dea6bb1a3091398a97
cristi <ccazacu81@gmail.com>
2020-02-18 09:30:11 +0200

    comit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class IndexController extends Controller
 {
     public function index() {
         $hl="Hello, Mady!";
         $ml="This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.";
-        return view ('index')->with(['hl'=>$hl,'ml'=>$ml]);
+        $articles=Article::all();
+        return view ('index')->with(['hl'=>$hl,'ml'=>$ml,'articles'=>$articles]);
+
     }
 }
